55a82ed0abab9cb0bd7d8c0de9d078885da9ba16
Bernard Bernstein <bernard@thebernsteins.com>
2025-01-03 11:50:34 -0500

    got code coverage above 80%

--- scripts/verify-local.sh
+++ scripts/verify-local.sh
@@ -8,7 +8,11 @@ cd frontend && npm run lint
 cd ..
 
 # Backend Go checks
-cd backend-go && golangci-lint run ./...
+cd backend-go
+golangci-lint run ./...
+go test -race ./...
+go test -coverprofile=test-results/coverage.out ./...
+go tool cover -html=test-results/coverage.out -o test-results/coverage.html
 cd ..
 
 # Terraform checks
@@ -17,3 +21,4 @@ terraform fmt -check -recursive
 tflint --init
 tflint --recursive
 cd ../..
+
