89ae6a39a1dd2a1d6941823c35da902674a2c8d6
Bernard Bernstein <bernard@thebernsteins.com>
2025-01-08 12:34:03 +0000

    Fixed dev.sh script Also cleaned up local dynamo script

--- scripts/init-local-dynamo.sh
+++ scripts/init-local-dynamo.sh
@@ -2,16 +2,11 @@
 
 export PAGER=""
 
-# Create stations cache table with composite key
-aws dynamodb create-table \
-    --table-name stations-cache \
-    --attribute-definitions \
-        AttributeName=stationId,AttributeType=S \
-    --key-schema \
-        AttributeName=stationId,KeyType=HASH \
-    --provisioned-throughput \
-        ReadCapacityUnits=5,WriteCapacityUnits=5 \
-    --endpoint-url http://localhost:8000
+# check if dynamodb table tide-prediction-cache exists
+if aws dynamodb describe-table --table-name tide-predictions-cache --endpoint-url http://localhost:8000 > /dev/null 2>&1; then
+    echo "Table tide-predictions-cache already exists. Skipping table creation."
+    exit 0
+fi
 
 # Create tide predictions cache table with composite key
 aws dynamodb create-table \
@@ -24,12 +19,6 @@ aws dynamodb create-table \
         AttributeName=date,KeyType=RANGE \
     --provisioned-throughput \
         ReadCapacityUnits=5,WriteCapacityUnits=5 \
-    --endpoint-url http://localhost:8000
-
-# Enable TTL for all tables
-aws dynamodb update-time-to-live \
-    --table-name stations-cache \
-    --time-to-live-specification "Enabled=true, AttributeName=ttl" \
     --endpoint-url http://localhost:8000
 
 aws dynamodb update-time-to-live \
